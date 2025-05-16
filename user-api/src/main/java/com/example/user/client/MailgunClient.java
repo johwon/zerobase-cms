0fabfa4bacb1d9f2efcffb8eff84c93378fe2bdf
johwon <wwalxxn@naver.com>
2025-05-16 21:16:59 +0900

    feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록 반드시 필요.

---
+++ user-api/src/main/java/com/example/user/client/MailgunClient.java
@@ -0,0 +1,13 @@
+package com.example.user.client;
+
+import com.example.user.client.mailgun.SendMailForm;
+import feign.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "mailgun", url="https://api.mailgun.net/v3/")
+public interface MailgunClient {
+    @PostMapping("sandboxb072ef3fd20e4dc3a400833c672d701f.mailgun.org/messages")
+    Response sendEmail(@SpringQueryMap SendMailForm form);
+}
