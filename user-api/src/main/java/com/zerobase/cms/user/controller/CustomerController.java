34333198166c31fc50fdfb0e395143ae4635fa92
johwon <wwalxxn@naver.com>
2025-05-22 14:27:40 +0900

    feat : [006-CUSTOMER-BALANCE] 고객에 대한 잔액 조회 구현

--- user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
+++ user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
@@ -2,18 +2,17 @@ package com.zerobase.cms.user.controller;
 
 import com.zerobase.cms.domain.config.JwtAuthenticationProvider;
 import com.zerobase.cms.domain.domain.common.UserVo;
+import com.zerobase.cms.user.domain.customer.ChangeBalanceForm;
 import com.zerobase.cms.user.domain.customer.CustomerDto;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.exception.CustomerException;
+import com.zerobase.cms.user.service.customer.CustomerBalanceService;
 import com.zerobase.cms.user.service.customer.CustomerService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.zerobase.cms.user.exception.ErrorCode.NOT_FOUND_USER;
 
@@ -23,8 +22,8 @@ import static com.zerobase.cms.user.exception.ErrorCode.NOT_FOUND_USER;
 public class CustomerController {
 
     private final JwtAuthenticationProvider provider;
-    private final CustomerService service;
     private final CustomerService customerService;
+    private final CustomerBalanceService customerBalanceService;
 
     @GetMapping("/getInfo")
     public ResponseEntity<CustomerDto> getInfo(@Parameter(in = ParameterIn.HEADER, description = "Access Token", required = true)
@@ -36,5 +35,11 @@ public class CustomerController {
         return ResponseEntity.ok(CustomerDto.form(c));
     }
 
+    @PostMapping("/balance")
+    public ResponseEntity<Integer> changeBalance(@RequestHeader(name="X-AUTH-TOKEN") String token,
+                                                 @RequestBody ChangeBalanceForm form){
+        UserVo vo = provider.getUserVo(token);
+        return ResponseEntity.ok(customerBalanceService.changeBalance(vo.getId(), form).getChangeMoney());
+    }
 
 }
---
+++ user-api/src/main/java/com/zerobase/cms/user/service/customer/CustomerBalanceService.java
@@ -0,0 +1,46 @@
+package com.zerobase.cms.user.service.customer;
+
+import com.zerobase.cms.user.domain.customer.ChangeBalanceForm;
+import com.zerobase.cms.user.domain.model.CustomerBalanceHistory;
+import com.zerobase.cms.user.domain.repository.CustomerBalanceHistoryRepository;
+import com.zerobase.cms.user.domain.repository.CustomerRepository;
+import com.zerobase.cms.user.exception.CustomerException;
+import com.zerobase.cms.user.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerBalanceService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerBalanceHistoryRepository customerBalanceHistoryRepository;
+
+    @Transactional(noRollbackFor = {CustomerException.class})
+    public CustomerBalanceHistory changeBalance(Long customerId, ChangeBalanceForm form) throws CustomerException{
+        CustomerBalanceHistory customerBalanceHistory =
+                customerBalanceHistoryRepository.findFirstByCustomer_IdOrderByIdDesc(customerId)
+                        .orElse(CustomerBalanceHistory.builder()
+                                .changeMoney(0)
+                                .currentMoney(0)
+                                .customer(customerRepository.findById(customerId)
+                                        .orElseThrow(()->new CustomerException(ErrorCode.NOT_FOUND_USER)))
+                        .build());
+        if(customerBalanceHistory.getCurrentMoney() + form.getMoney() < 0){
+            throw new CustomerException(ErrorCode.NOT_ENOUGH_BALANCE);
+        }
+
+        customerBalanceHistory = CustomerBalanceHistory.builder()
+                .changeMoney(form.getMoney())
+                .currentMoney(customerBalanceHistory.getCurrentMoney()+form.getMoney())
+                .description(form.getMessage())
+                .fromMessage(form.getFrom())
+                .customer(customerBalanceHistory.getCustomer())
+                .build();
+        customerBalanceHistory.getCustomer().setBalance(customerBalanceHistory.getCurrentMoney());
+
+        return customerBalanceHistoryRepository.save(customerBalanceHistory);
+    }
+
+}
